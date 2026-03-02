6249b855bff5c10999fa0107d39c5cb7bcfde998
Jinner <djinner@gmail.com>
2026-03-02 16:11:27 -0500

    Host Visible

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,15 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 
 export default defineConfig({
+    vite: {
+        server: {
+            allowedHosts: ['link.iiap.org.co', 'localhost', '127.0.0.1'],
+        },
+    },
+    server: {
+        host: true,
+        port: 4321
+    },
     output: "server",
     adapter: node({
         mode: "standalone"
