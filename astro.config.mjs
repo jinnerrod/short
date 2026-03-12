a26acd688f95bf3a6ba7289f209d2de4cba74ffe
Jinner <djinner@gmail.com>
2026-03-12 18:58:20 -0500

    Ajuste del package.json y el astra.conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import node from "@astrojs/node";
 export default defineConfig({
     vite: {
         server: {
-            allowedHosts: ['furag.iiap.org.co','link.iiap.org.co', 'localhost', '127.0.0.1'],
+            allowedHosts: ['furag.iiap.org.co', 'link.iiap.org.co', 'localhost', '127.0.0.1'],
+        },
+        ssr: {
+            noExternal: true,
         },
     },
     server: {
